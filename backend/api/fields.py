8f2a5de2994858f47dfe6809c69a6546a27df0b5
Artur Kostenkov <turiking@yandex.ru>
2023-05-23 09:58:18 +0300

    fix5

--- backend/api/fields.py
+++ backend/api/fields.py
@@ -9,6 +9,6 @@ class Hex2NameColor(serializers.Field):
     def to_internal_value(self, data):
         try:
             data = webcolors.hex_to_name(data)
-            return data
         except ValueError:
             raise serializers.ValidationError('Для этого цвета нет имени')
+        return data
